9c739fb9de0c641436273879991faad2c528db36
Ruben Martinez <therubenm@gmail.com>
2021-07-18 22:36:22 -0500

    Deploy to the Internet using Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hi: 'there. Deployed and Ready.'});
 });
 
 // Dynamic Port Binding. Written this way to account for dev and prod.
