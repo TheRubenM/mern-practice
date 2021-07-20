7b34ac21a86a9329483e1168d12d73e0871df0ad
Ruben Martinez <therubenm@gmail.com>
2021-07-20 09:54:37 -0500

    Set up Google OAuth and implement Passport

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+require('./services/passport');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there. Deployed and Ready.'});
-});
+require('./routes/authRoutes')(app);
 
 // Dynamic Port Binding. Written this way to account for dev and prod.
 const PORT = process.env.PORT || 5000;
