1b9d25051cbd8ddc6a48187ae2ff0bc237e0ceac
Ruben Martinez <therubenm@gmail.com>
2021-07-21 02:32:08 -0500

    Prepare for MongoDB setup

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('./services/passport');
 const app = express();
 
 require('./routes/authRoutes')(app);
+mongoose.connect(keys.mongoURI);
 
 // Dynamic Port Binding. Written this way to account for dev and prod.
 const PORT = process.env.PORT || 5000;
